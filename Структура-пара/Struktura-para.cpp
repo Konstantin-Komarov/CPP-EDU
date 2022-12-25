3579dc32462bd815b1e517a65e05fe1649a4d762
Konstantin-Komarov <120031241+Konstantin-Komarov@users.noreply.github.com>
2022-12-25 19:05:44 +0300

    Add files via upload

---
+++ Структура-пара/Struktura-para.cpp
@@ -0,0 +1,29 @@
+﻿#include <iostream>
+#include "Структура-пара.h"
+
+using namespace std;
+
+int main()
+{
+	setlocale(LC_ALL, "RUS");
+	float cost = 2.99;
+	int num = 20;
+
+	//Используем конструктор инициализации
+	Products prod(cost, num);
+
+	//Выводим элемент класса
+	prod.display(prod);
+	
+	//Используем конструктор копирования
+	Products prod1(prod);
+
+	//Создаем элемент кдасса через чтение
+	prod1.read(prod1);
+
+	//Выводим элемент класса
+	prod1.display(prod1);
+
+	//Вычисляем стоимость товаров
+	prod.cost(prod1);
+}
