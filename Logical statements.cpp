82481bc7d5004278fed29f48f0bee87e9c99a59e
Konstantin-Komarov <120031241+Konstantin-Komarov@users.noreply.github.com>
2022-12-25 19:14:14 +0300

    Add files via upload

---
+++ Logical statements.cpp
@@ -0,0 +1,14 @@
+﻿#include <iostream>
+
+using namespace std;
+int main()
+{
+	setlocale(LC_ALL, "RUS");
+	int A;
+	int B;
+	cout << "Введите число А: ";
+	cin >> A;
+	cout <<endl<< "Введите число B: ";
+	cin >> B;
+	cout << "Логическое высказывание = " << ((A >= 0) || (B < -2));
+}
